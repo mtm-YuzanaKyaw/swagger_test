be353159e533d9d1df4f2f9ccab9f3d66338d9db
mtm-YuzanaKyaw <mtm.yuzanakyaw@gmail.com>
2025-01-07 14:43:32 +0630

    logout user api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: "", path_names: {
     sign_in: "login",
-    sigin_out: "logout",
+    sign_out: "logout",
     registration: "signup"
   },
   controllers: {
